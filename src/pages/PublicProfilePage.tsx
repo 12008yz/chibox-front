135ea47f80977716564ed140232364e4a3ccb912
Денис <deniscikasov@gmail.com>
2025-08-14 20:48:32 +0300

    Общая стоимость предметов и best weapon в профиле

--- src/pages/PublicProfilePage.tsx
+++ src/pages/PublicProfilePage.tsx
@@ -340,7 +340,7 @@ const PublicProfilePage: React.FC = () => {
                 <path fillRule="evenodd" d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z" clipRule="evenodd" />
               </svg>
             </div>
-            Лучшее выбитое оружие
+            Рекорд за всё время
           </h3>
 
           {bestWeapon ? (
@@ -362,7 +362,14 @@ const PublicProfilePage: React.FC = () => {
                   </div>
                 </div>
                 <div className="flex-1">
-                  <h4 className="text-lg font-bold text-white mb-2">{bestWeapon.name}</h4>
+                  <h4 className="text-lg font-bold text-white mb-2">
+                    {bestWeapon.name}
+                    {bestWeapon.isRecord && (
+                      <span className="ml-2 px-2 py-1 bg-yellow-500 text-black text-xs font-bold rounded-full">
+                        РЕКОРД
+                      </span>
+                    )}
+                  </h4>
                   <div className="flex items-center gap-4 mb-2">
                     <span className={`px-3 py-1 rounded-full text-xs font-semibold bg-gradient-to-r ${getRarityColor(bestWeapon.rarity)} text-white`}>
                       {getRarityName(bestWeapon.rarity)}
@@ -371,6 +378,9 @@ const PublicProfilePage: React.FC = () => {
                   </div>
                   <p className="text-gray-400 text-sm">
                     Тип: {bestWeapon.weapon_type || 'Оружие'}
+                    {bestWeapon.isRecord && (
+                      <span className="ml-2 text-yellow-400 text-xs">(рекордная цена)</span>
+                    )}
                   </p>
                 </div>
               </div>
@@ -382,7 +392,7 @@ const PublicProfilePage: React.FC = () => {
                   <path fillRule="evenodd" d="M10 2L3 7v6l7 5 7-5V7l-7-5zM6.5 9.5 9 11l2.5-1.5L14 8l-4-2.5L6 8l.5 1.5z" clipRule="evenodd" />
                 </svg>
               </div>
-              <p className="text-gray-400 text-sm">Пока нет выбитого оружия</p>
+              <p className="text-gray-400 text-sm">Пока не установлен рекорд</p>
             </div>
           )}
         </div>
